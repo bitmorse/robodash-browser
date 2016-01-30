81947da363b0eacab07f0ce950ceeb5639157ae9
Sam Sulaimanov <sam@bitmorse.com>
2016-01-30 15:41:18 +0100

    prior to ember pouch

---
+++ electron-builds/Robodash-linux-x64/resources/app/electron.js
@@ -0,0 +1,77 @@
+/* jshint node: true */
+
+var electron = require('electron');
+
+var app = electron.app;
+var mainWindow = null;
+var BrowserWindow = electron.BrowserWindow;
+
+var Cylon = require('cylon');
+
+
+
+electron.crashReporter.start();
+
+app.on('window-all-closed', function onWindowAllClosed() {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function onReady() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    delete mainWindow.module;
+
+    // If you want to open up dev tools programmatically, call
+    mainWindow.openDevTools();
+
+    // By default, we'll open the Ember App by directly going to the
+    // file system.
+    //
+    // Please ensure that you have set the locationType option in the
+    // config/environment.js file to 'hash'. For more information,
+    // please consult the ember-electron readme.
+    mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
+
+
+
+    Cylon.robot({
+       name: "octanis1_rover",
+       connections: {
+         octanis1_rover: {adaptor: 'octanis1-rover'},
+         bluetooth: { adaptor: 'ble', uuid: 'cbef787e50934150ace41ce6385efabf'}
+       },
+
+       devices: {
+        rover_ble: {
+          driver: "ble_cli",
+          serviceId: "ffe0", characteristicId: "ffe1",
+          connection: "bluetooth"
+        },
+
+        rover_joystick: {
+          driver: "joystick",
+          connection: "octanis1_rover"
+        }
+      }
+
+    });
+
+    Cylon.api('socketio',{
+      host: '0.0.0.0', // CHANGE THIS!!!
+      port: '3000'
+    });
+
+    Cylon.start();
+
+
+
+
+    mainWindow.on('closed', function onClosed() {
+        mainWindow = null;
+    });
+});
