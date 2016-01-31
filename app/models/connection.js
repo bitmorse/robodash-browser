cdcc2aaef715bcf8b534e4bfecbf20f8b5fea4ff
Sam Sulaimanov <sam@bitmorse.com>
2016-01-31 17:52:51 +0100

    adding belongsTo

--- app/models/connection.js
+++ app/models/connection.js
@@ -4,5 +4,6 @@ import { Model } from 'ember-pouch';
 export default Model.extend({
   name: DS.attr('string'),
   type: DS.attr('string'),
-  opts: DS.attr('string')
+  opts: DS.attr('string'),
+  machine: DS.belongsTo('machine')
 });
